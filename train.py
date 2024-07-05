b85725b20e969cf28a48bf63633803e8a6affdc4
darshil929 <darshilt929@gmail.com>
2024-07-06 03:18:36 +0530

    hyperparameters

--- train.py
+++ train.py
@@ -10,11 +10,13 @@ from model import Discriminator, Generator, initialize_weights
 
 # Hyperparameters etc.
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 2e-4
-BATCH_SIZE = 128
+LEARNING_RATE = 5e-5 # here we can even use 2 different learning rates for the generator and the discriminator 
+BATCH_SIZE = 64
 IMAGE_SIZE = 64
 CHANNELS_IMG = 1
 Z_DIM = 100
 NUM_EPOCHS = 5
 FEATURES_DISC = 64
 FEATURES_GEN = 64
+CRIRIC_ITERATIONS = 5
+WEIGHT_CLIP = 0.01
