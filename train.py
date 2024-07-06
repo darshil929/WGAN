4e990caceb878e5273dde48c7468b4cfad9ebffa
darshil929 <darshilt929@gmail.com>
2024-07-06 15:24:40 +0530

    update train.py

--- train.py
+++ train.py
@@ -7,10 +7,11 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from model import Discriminator, Generator, initialize_weights
+from utils import gradient_penalty
 
 # Hyperparameters etc.
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 5e-5 # here we can even use 2 different learning rates for the generator and the discriminator 
+LEARNING_RATE = 1e-4 # here we can even use 2 different learning rates for the generator and the discriminator 
 BATCH_SIZE = 64
 IMAGE_SIZE = 64
 CHANNELS_IMG = 1
@@ -19,7 +20,7 @@ NUM_EPOCHS = 5
 FEATURES_DISC = 64
 FEATURES_GEN = 64
 CRIRIC_ITERATIONS = 5
-WEIGHT_CLIP = 0.01
+LAMBDA_GP = 10
 
 transforms = transforms.Compose(
     [
@@ -38,8 +39,8 @@ critic = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
 initialize_weights(gen)
 initialize_weights(critic)
 
-opt_gen = optim.RMSprop(gen.parameters(), lr=LEARNING_RATE)
-opt_critic = optim.RMSprop(critic.parameters(), lr=LEARNING_RATE)
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+opt_critic = optim.Adam(critic.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
 
 fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
 writer_real = SummaryWriter(f"logs/real")
@@ -54,14 +55,13 @@ for epoch in range(NUM_EPOCHS):
             fake = gen(noise)
             critic_real = critic(real).reshape(-1)
             critic_fake = critic(fake).reshape(-1)
-            loss_critic = -(torch.mean(critic_real) - torch.mean(critic_fake))
+            gp = gradient_penalty(critic, real, fake, device=device)
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
+            )
             critic.zero_grad()
             loss_critic.backward(retain_graph=True)
             opt_critic.step()
-
-            # Cliping weights
-            for p in critic.parameters():
-                p.data.clamp_(-WEIGHT_CLIP, WEIGHT_CLIP)
 
         ### Train Generator: min -E[critic(gen_fake)]
         gen_fake = critic(fake)
